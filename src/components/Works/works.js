147d505c6d00c5adfa3d7c9495643b6574a04d41
Hitesh Ram <hiteshram321@gmail.com>
2023-11-19 16:10:09 +0530

    changes made

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -13,12 +13,12 @@ const Works = () => {
         <h2 className='workstitle'>My Portfolio</h2>
         <span className='worksDesc'>Welcome to my machine learning portfolio! I'm Hitesh Ram, showcasing a collection of innovative projects in predictive analytics, deep learning marvels, and artificial neural networks. Dive into a world where data meets creativity, highlighting my expertise in pushing the boundaries of machine learning.</span>
         <div className='worksImgs'>
-            <img src = {portfolio1} alt='' className='worksImg'/>
-            <img src={portfolio2} alt='' className='worksImg'/>
-            <img src={portfolio3} alt='' className='worksImg'/>
-            <img src={portfolio4} alt='' className='worksImg'/>
-            <img src={portfolio5} alt='' className='worksImg'/>
-            <img src={portfolio6} alt='' className='worksImg'/>
+            <a href='https://github.com/HiteshRam666/Lip-Reading-web-app-using-ML' target="_blank" rel="noopener noreferrer"><img src = {portfolio1} alt='' className='worksImg'/></a>
+            <a href='https://github.com/HiteshRam666/End-to-End-Potato-Leave-Disease-Detection/tree/master' target="_blank" rel="noopener noreferrer"><img src = {portfolio2} alt='' className='worksImg'/></a>
+            <a href='https://github.com/HiteshRam666/End-to-End-Malaria-Cell-Detection-using-CNN' target="_blank" rel="noopener noreferrer"><img src = {portfolio3} alt='' className='worksImg'/></a>
+            <a href='https://github.com/HiteshRam666/Food-Prediction-WebApp-using-Tensorflow' target="_blank" rel="noopener noreferrer"><img src = {portfolio4} alt='' className='worksImg'/></a>
+            <a href='https://github.com/HiteshRam666/MovieX-Fully-Responsive-website-using-React-Js' target="_blank" rel="noopener noreferrer"><img src = {portfolio5} alt='' className='worksImg'/></a>
+            <a href='https://github.com/HiteshRam666/Pothole-Detection-using-Yolov8-' target="_blank" rel="noopener noreferrer"><img src = {portfolio6} alt='' className='worksImg'/></a>
         </div>
         <button className='worksBtn'>See More</button>
     </section>

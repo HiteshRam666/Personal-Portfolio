0cf6a8a65996fd735ced4d324f8bb1fff7fbfe39
Hitesh Ram <hiteshram321@gmail.com>
2023-11-19 12:57:05 +0530

    footer and menu added

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,11 @@
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+        Copyright &#169, 2023 Hitesh Ram, All Rights Recieved.
+    </footer>
+  )
+}
+
+export default Footer
